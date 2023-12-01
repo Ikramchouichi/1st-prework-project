7be9cfe1878152cf93fac1e7a06a24143fc66425
Ikram Chouichi <ikram@netronian.com>
2023-12-01 16:21:49 -0500

    changes in javascript

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+document.getElementById("toggleBio").addEventListener("click", function() {
+    var moreText = document.getElementById("moreBio");
+    var btnText = this;
+
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline";
+      btnText.textContent = "Show Less";
+    } else {
+      moreText.style.display = "none";
+      btnText.textContent = "Show More";
+    }
+  });
