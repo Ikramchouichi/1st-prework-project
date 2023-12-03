a4e8b6d30526330c1eddf830710d02db86ebbfc4
Ikram Chouichi <ikram@netronian.com>
2023-12-03 13:49:52 -0500

    added a JS listener - adjusted a few colors and styling issues

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+// h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 document.getElementById("toggleBio").addEventListener("click", function() {
     var moreText = document.getElementById("moreBio");
@@ -13,3 +13,16 @@ document.getElementById("toggleBio").addEventListener("click", function() {
       btnText.textContent = "Show More";
     }
   });
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var h3Elements = document.querySelectorAll("h3");
+
+    h3Elements.forEach(function(h3, index) {
+
+        setTimeout(function() {
+            h3.style.opacity = 1;
+            h3.style.transform = "translateY(0)";
+        }, 1000 * index);
+    });
+});
